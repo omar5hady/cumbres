d7ad29575aa8d99c1ba40f2a6e48b57042648702
juanguzman1 <juanltd07@gmail.com>
2019-02-05 09:06:12 -0600

    cambios de login edceteda

--- resources/views/auth/contenido.blade.php
+++ resources/views/auth/contenido.blade.php
@@ -5,11 +5,11 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="Sistema Ventas Laravel Vue Js- IncanatoIT">
-  <meta name="author" content="Incanatoit.com">
-  <meta name="keyword" content="Sistema ventas Laravel Vue Js, Sistema compras Laravel Vue Js">
+  <meta name="description" content="Sistema cumbres">
+  <meta name="author" content="casascumbres.mx">
+  <meta name="keyword" content="Sistema de venta de casas ">
 
-  <title>Sistema Ventas - IncanatoIT</title>
+  <title>Sistema cumbres</title>
 
   <!-- Icons -->
   <link href="vendors/css/font-awesome.min.css" rel="stylesheet">
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,9 +31,8 @@
           <div class="card text-white bg-primary py-5 d-md-down-none" style="width:44%">
             <div class="card-body text-center">
               <div>
-                <h2>Sistema de Ventas IncanatoIT</h2>
-                <p>Sistema de compras, Ventas desarrollado en PHP utilizando el Framework Laravel y Vue Js, con el gestor de base de datos MariaDB.</p>
-                <a href="https://www.udemy.com/user/juan-carlos-arcila-diaz/" target="_blank" class="btn btn-primary active mt-3">Ver el curso!</a>
+                <h2>Sistema cumbres</h2>
+                <p>sistema de venta de casas - Grupo constructor cumbres</p>
               </div>
             </div>
           </div>
