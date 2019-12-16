76b8c3e9db747c1e30b01227ae0613c7f0b1186a
omar5hady <omar_vazquez_7@hotmail.com>
2019-12-16 10:47:28 -0600

    doble clic en solic de equipamientos para ver detalles

--- resources/assets/js/components/Ventas/Equipamientos.vue
+++ resources/assets/js/components/Ventas/Equipamientos.vue
@@ -76,7 +76,7 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr v-for="contratos in arrayContratos" :key="contratos.folio">
+                                    <tr v-for="contratos in arrayContratos" @dblclick="historial = 1, listarHistorial(1,contratos.folio,'','','','contratos.id');" :key="contratos.folio">
                                         <template>
                                             <td class="td2" v-text="contratos.folio"></td>
                                             <td class="td2" v-text="contratos.nombre_cliente"></td>
