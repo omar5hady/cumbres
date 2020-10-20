fa32bb35c3e67fc89237e6918818ec72ca8afaec
Rafael Rivera <rafael.rivh@gmail.com>
2020-10-20 13:41:58 -0500

    agregado de campo para factura de concretania modulo Saldos->Facturas->Creditos Directos

--- database/migrations/2020_02_07_095432_add_publicidad_id_to_contratos.php
+++ database/migrations/2020_02_07_095432_add_publicidad_id_to_contratos.php
@@ -21,6 +21,11 @@ class AddPublicidadIdToContratos extends Migration
             $table->string('e_folio_factura',30)->nullable(); //factura de firma de escrituras
             $table->double('e_monto', 8,2)->nullable()->default(0); //factura de firma de escrituras
             $table->date('e_f_carga_factura')->nullable(); //factura de firma de escrituras
+
+            $table->string('e_factura_concretania')->nullable();
+            $table->string('e_folio_factura_concretania',30)->nullable();
+            $table->double('e_monto_concretania', 8,2)->nullable()->default(0);
+            $table->date('e_f_carga_factura_concretania')->nullable();
         });
     }
 
