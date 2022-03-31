99620d980c4cfbc83b60420238b72543f20f9be3
omar5hady <omar_vazquez_7@hotmail.com>
2022-03-31 09:43:19 -0600

    Cambio de permisos en modulo digital leads y actualizacion de vistas para errores

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -12,80 +12,196 @@
     <meta charset="utf-8">
     <title>Sistema Cumbres</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+	<link href="css/plantilla.css" rel="stylesheet">
     
 
 </head>
-  <body class="app flex-row align-items-center">
-		<div class="wrapper">
-				<div class="box">
-				<h1>500</h1>
-				<p>Sorry, it's me, not you.</p>
-				<p>&#58;&#40;</p>
-				<p><a href="/">Let me try again!</a></p>
-				</div>
-				</div>
+  <body>
+	<h5>Internal Server error !</h5>
+    <h1>5</h1>
+    <h1>00</h1>
+    <div class="box">
+			<span></span><span></span>
+			<span></span><span></span>
+			<span></span>
+		</div>
+    <div class="box">
+			<span></span><span></span>
+			<span></span><span></span>
+			<span></span>
+		</div>
+    <p> We're unable to find out what's happening! We suggest you to
+			<br/>
+			<a href="#">Go Back</a>
+			or visit here later.</p>
   </body>
 </html>
 
 <style>
-	html { box-sizing: border-box; }
-
 	*,
-	*::before,
-	*::after { box-sizing: inherit; }
+*::after,
+*::before {
+	box-sizing: border-box;
+}
 
-	body * {
+html,
+body {
+	align-items: center;
+	background: linear-gradient(#003eff, #0028a9);
+	color: white;
+	display: flex;
+	font: 2rem "Poiret One";
+	height: 100vh;
+	justify-content: center;
 	margin: 0;
 	padding: 0;
-	}
+}
 
-	body {
-	font: normal 100%/1.15 "Merriweather", serif;
-	background-color: #7ed0f2;
-	color: #fff;
-	}
-
-	.wrapper {
+.box {
+	height: 100px;
+	margin: 0 10px;
+	overflow: hidden;
 	position: relative;
-	max-width: 1298px;
-	height: auto;
-	margin: 2em auto 0 auto;
-	}
+	transform: rotateZ(270deg) scale(1.05);
+	width: 100px;
+}
 
-	/* https://www.flaticon.com/authors/vectors-market */
-	/* https://www.flaticon.com/authors/icomoon */
-	.box {
-	max-width: 70%;
-	min-height: auto;
+.box:nth-of-type(2) {
+	left: -28px;
+	transform: rotateX(-180deg) rotateY(180deg) rotateZ(270deg) scale(1.05);
+}
+
+.box span {
+	animation: loader 4.8s infinite both;
+	display: block;
+	height: 100%;
+	position: absolute;
+	width: 100%;
+}
+
+.box span:nth-child(1) {
+	animation-delay: 0.2s;
+}
+
+.box span:nth-child(2) {
+	animation-delay: 0.4s;
+}
+
+.box span:nth-child(3) {
+	animation-delay: 0.6s;
+}
+
+.box span:nth-child(4) {
+	animation-delay: 0.8s;
+}
+
+.box span:nth-child(5) {
+	animation-delay: 1s;
+}
+
+.box span:nth-child(6) {
+	animation-delay: 1.2s;
+}
+
+.box span::after {
+	background: #fff;
+	border-radius: 50%;
+	content: "";
+	left: 50%;
+	padding: 6px;
+	position: absolute;
+	top: 0;
+	transform: translateX(-50%);
+}
+
+@keyframes loader {
+	0% {
+		opacity: 0;
+		transform: rotate(180deg);
+		animation-timing-function: ease-out;
+	}
+	5% {
+		opacity: 1;
+		transform: rotate(300deg);
+		animation-timing-function: linear;
+	}
+	30% {
+		transform: rotate(420deg);
+		animation-timing-function: ease-in-out;
+	}
+	35% {
+		transform: rotate(625deg);
+		animation-timing-function: linear;
+	}
+	70% {
+		transform: rotate(800deg);
+		animation-timing-function: ease-out;
+		opacity: 1;
+	}
+	75% {
+		opacity: 0;
+		transform: rotate(900deg);
+		animation-timing-function: ease-out;
+	}
+	76% {
+		opacity: 0;
+		transform: rotate(900deg);
+	}
+	100% {
+		opacity: 0;
+		transform: rotate(900deg);
+	}
+}
+
+h1 {
+	text-shadow: 0 0 10px #fff;
+	animation: blink 4.8s infinite both;
+}
+
+h1:nth-of-type(2) {
+	animation: none;
+	letter-spacing: -14px;
 	margin: 0 auto;
-	padding: 1em 1em;
+	opacity: 0.1;
+	padding-left: 41px;
+	position: absolute;
+	top: 50%;
+	transform: translatey(-50%);
+}
+
+@keyframes blink {
+	0%,
+	50%,
+	100% {
+		opacity: 0.2;
+	}
+	25%,
+	75% {
+		opacity: 1;
+	}
+}
+
+h5,
+p {
+	position: absolute;
+	font-family: monospace;
+}
+
+h5 {
+	top: 10%;
+	font-size: 0.4em;
+	
+}
+
+p {
+	font-size: 0.3em;
+	bottom: 10%;
+	width: 50%;
 	text-align: center;
-	background: url("https://www.dropbox.com/s/xq0841cp3icnuqd/flame.png?raw=1") no-repeat top 10em center/128px 128px,
-				transparent url("https://www.dropbox.com/s/w7qqrcvhlx3pwnf/desktop-pc.png?raw=1") no-repeat top 13em center/128px 128px;
-	}
+}
 
-	h1, p:not(:last-of-type) { text-shadow: 0 0 6px #216f79; }
+p a {
+	color: cyan;
+}
 
-	h1 {
-	margin: 0 0 1rem 0;
-	font-size: 8em;
-	}
-
-	p {
-	margin-bottom: 0.5em;
-	font-size: 3em;
-	}
-
-	p:first-of-type { margin-top: 4em; }
-
-	p > a {
-	border-bottom: 1px dashed #216f79;
-	font-style: italic;
-	text-decoration: none;
-	color: #216f79;
-	}
-
-	p > a:hover { text-shadow: 0 0 6px #216f79; }
-
-	p img { vertical-align: bottom; }
 </style>
