29a2c549edb0bccfce4cf68a3f63a5363c978129
omar5hady <omar_vazquez_7@hotmail.com>
2022-08-08 15:50:27 -0500

    Modulo Fondo de ahorro v1

--- app/User.php
+++ app/User.php
@@ -130,7 +130,6 @@ class User extends Authenticatable
         'admin_mant_vehiculos',
         'prestamos_personales',
         'fondo_ahorro',
-        'prestamos_personales',
 
         //Oficina
         'inventarios',
