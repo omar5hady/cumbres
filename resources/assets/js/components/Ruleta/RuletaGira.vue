0f425b756ea352173f487e88cf29fb13ad207d7e
omar5hady <omar_vazquez_7@hotmail.com>
2023-02-21 15:59:38 -0600

    Cambios ruleta vista modal y ajustes leads

--- resources/assets/js/components/Ruleta/RuletaGira.vue
+++ resources/assets/js/components/Ruleta/RuletaGira.vue
@@ -2,9 +2,9 @@
 <div class="conte_principal">
 
       <div class="content_grid">
-        
+
         <div class="top">
-          
+
         </div>
 
         <div v-if="!muestraPremio"  class="div_tr">
@@ -18,13 +18,13 @@
         </div>
         <div  v-if="muestraPremio" class="div_fanfar_izq" >
             <img  src="/img/ruleta/fanfarria1_Izq.png" width="100%" height="100%"  alt="">
-         
+
         </div>
         <div v-if="muestraPremio"  class="div_fanfar_der" >
           <img  src="/img/ruleta/fanfarria2_Der.png" width="100%" height="100%"  alt="">
-        
+
         </div>
-         
+
         <div  v-if="muestraPremio" class="grid_ruleta">
             <img v-if="vP =='3'" class="content_ruleta" style=" z-index: 5;" src="/img/ruleta/3mil.png" alt="">
             <img v-if="vP =='5'" class="content_ruleta" style=" z-index: 5;" src="/img/ruleta/5mil.png" alt="">
@@ -32,10 +32,10 @@
             <img v-if="vP =='10'" class="content_ruleta" style=" z-index: 5;" src="/img/ruleta/10mil.png" alt="">
             <img v-if="vP =='15'" class="content_ruleta" style=" z-index: 5;" src="/img/ruleta/15mil.png" alt="">
             <img v-if="vP =='0'" class="content_ruleta" style=" z-index: 5;" src="/img/ruleta/GraciasxParticipar.png" alt="">
-         
+
         </div>
-       
-      
+
+
         <div v-if="div_registra" class="div_registra_d">
           <div class="form_registra">
               <label for="nom"> Nombre(s) </label>
@@ -48,21 +48,21 @@
               </div>
               <label  for="f_nac"> Fecha de nacimiento </label>
               <input v-model="fn" class="form-control" style="font-size: x-large; border-radius: 10px;" type="date" name="" id="f_nac">
-              
+
                <div v-if="validDatos" class="mensaje-error" style="font-size: 16px; padding-top: 30px; margin-left: 100px;">Llene todos los campos requeridos.</div>
 
           </div>
-         
+
           <a class="aviso_priv" style=" margin-left:220px;" target="_blank"  href="https://www.casascumbres.mx/wp-content/uploads/2022/09/AvisoPrivacidad2022Nom-1.pdf">Aviso de privacidad</a>
         </div>
           <div v-if="div_registra" class="div_boton_gl">
               <img style="width: 240px; height: 50px;" src="/img/ruleta/BuenaSuerte_boton.png" @click="btn_gl()" alt="">
-          </div>  
-          
+          </div>
+
 
           <div v-if="btn_girar" class="div_girar">
               <img class="img_girar" src="/img/ruleta/boton_girar.png" @click="registraD()">
-          </div> 
+          </div>
 
           <div v-if="form_input_cw" class="div_legenda">
                <img  src="/img/ruleta/donde_recibir.png">
@@ -73,17 +73,19 @@
           </div>
 
           <div v-if="btn_email || btn_waths" class="div_corr_wts">
-             
+
              <div v-if="btn_email" class="form_corr_wts">
               <!-- <label  for="correo">Correo</label> -->
               <input v-model="reg_correo" placeholder="Correo electronico" class="form-control" style="font-size: x-large; font-family: sans-serif; border-radius: 10px;" type="text" name="" id="correo" >
               <a class="aviso_priv"  target="_blank"  href="https://www.casascumbres.mx/wp-content/uploads/2022/09/AvisoPrivacidad2022Nom-1.pdf">Aviso de privacidad</a>
-            </div> 
+              <label style="font-size:14px; color: yellow">* Recibirás tu cupón en un lapso de 24 horas *</label>
+            </div>
 
             <div v-if="btn_waths" class="form_corr_wts">
               <!-- <label  for="cw">WhatsApp</label> -->
-              <input v-model="reg_whts" placeholder="Whats app" class="form-control" maxlength="10" style="font-size: x-large; font-family: sans-serif; border-radius: 10px;" type="text" name="" id="cws" v-on:keypress="isNumber($event)"> 
+              <input v-model="reg_whts" placeholder="Whats app" class="form-control" maxlength="10" style="font-size: x-large; font-family: sans-serif; border-radius: 10px;" type="text" name="" id="cws" v-on:keypress="isNumber($event)">
               <a class="aviso_priv"  target="_blank"  href="https://www.casascumbres.mx/wp-content/uploads/2022/09/AvisoPrivacidad2022Nom-1.pdf">Aviso de privacidad</a>
+              <label style="font-size:14px; color: yellow">* Recibirás tu cupón en un lapso de 24 horas *</label>
             </div>
 
           </div>
@@ -92,9 +94,9 @@
                   <img @click="putCorreoWhats()" style="width: 140px; height: 50px;" src="/img/ruleta/enviar_boton.png" alt="">
                   <div v-if="validCorreo" class="mensaje-error" style="font-size: 16px;  margin-left: -20px; width: 200px;">
                     Ingrese un correo válido.</div>
-            </div> 
+            </div>
 
-          
+
       </div>
 
 </div>
@@ -135,18 +137,18 @@ export default {
       validCorreo:false,
       validDatos:false,
 
-      
+
 
     }
   },
- 
+
 
   methods:{
 
     registraD(){
        if(this.no !='' && this.app !='' && this.apm !='' && this.fn !=''){
           this.postLead()
-          
+
       }else{
          this.div_registra=true;
       }
@@ -156,7 +158,7 @@ export default {
     },
 
     btn_gl(){
-      
+
 
       if(this.no !='' && this.app !='' && this.apm !='' && this.fn !=''){
         this.div_registra=false;
@@ -180,40 +182,40 @@ export default {
                   img_tr[0].style.animationIterationCount="135"
 
                 var element = $('.anim'); // acceso a las propiedades de CCS de la clase .anim
-              
+
                   switch (vlp) {
                     case '8':
                         element[0].style.animation="carousel8"
                         element[0].style.transform="rotate(7230deg)"
                       break;
-                  
+
                     case '5':
                         element[0].style.animation="carousel5"
                         element[0].style.transform="rotate(7260deg)"
-                      
+
                       break;
                     case '3':
                         element[0].style.animation="carousel3"
                         element[0].style.transform="rotate(7290deg)"
-                      
+
                       break;
                     case '0':
                         element[0].style.animation="carousel0"
                         element[0].style.transform="rotate(7320deg)"
-                      
+
                       break;
                     case '15':
                         element[0].style.animation="carousel15"
                         element[0].style.transform="rotate(7350deg)"
-                      
+
                       break;
                     case '10':
                         element[0].style.animation="carousel10"
                         element[0].style.transform="rotate(7380deg)"
-                      
+
                       break;
-                  
-                  
+
+
                     default:
                       break;
                   }
@@ -236,7 +238,7 @@ export default {
 
               setTimeout(()=>{
                   this.muestraPremio=true
-                  this.vP=vlp  
+                  this.vP=vlp
                   this.form_input_cw=true;
               },"8500")
 
@@ -372,39 +374,39 @@ export default {
                 var date = fechaN.split('');
                 var p2 = date[2]+date[3]+date[5]+date[6]+date[8]+date[9];
 
-              
+
                 return  p1 + p2;
             },
 
 
-    
+
     checkPremioFolio(folio,registrado) { // verifica el premio segun su folio y si ya esta registrado
                 // 3mil  1-9...  5mil 10-20 ,!=50 ... 8mil 50-150-250...  10mil 100, 200, !=1000 ...  15mil -1000 , 2000
 
             if (registrado != '1') {
                   var select_premio;
-                  var premDiezMil=[];     
-                  var premOchoMil=[];     
-                  var premCincoMil=[];     
-                  var premTresMil = [];    
-                  var premQuinceMil=[];    
-                  
-                  for (let index = 1; index <= 100; index++) { 
+                  var premDiezMil=[];
+                  var premOchoMil=[];
+                  var premCincoMil=[];
+                  var premTresMil = [];
+                  var premQuinceMil=[];
+
+                  for (let index = 1; index <= 100; index++) {
                       premQuinceMil.push(index*1000)
                   }
-                  for (let index = 1; index <= 100; index++) { 
+                  for (let index = 1; index <= 100; index++) {
                     if (((index*100)%1000) !=0){
                       premDiezMil.push(index*100)
-                      
+
                     }
                   }
 
                   for (let index = 1; index <= 100 ; index++) {
                     if ((index % 2) != 0) {
-                      premOchoMil.push(index *50)   
+                      premOchoMil.push(index *50)
                     }
                   }
-                  
+
                   for (var i = 0; i < 1000; i++) {
                     if ((i*10) % 50 !== 0 && (i*10) % 100 !== 0) {
                     premCincoMil.push(i*10);
@@ -415,7 +417,7 @@ export default {
                       premTresMil.push(i);
                     }
                   }
-      
+
                   select_premio = premTresMil.includes(folio);
                   if(!select_premio){
                   select_premio = premCincoMil.includes(folio);
@@ -441,26 +443,26 @@ export default {
                             this.premioCliente='8';
                             this.premio='8000';
                             this.descripcion='Premio de 8000 pesos ';
-                          }  
+                          }
                       }else{
                         this.premioCliente='5';
                         this.premio='5000';
                         this.descripcion='Premio de 5000 pesos ';
-                      }  
+                      }
                   }else {
                     this.premioCliente='3';
                     this.premio='3000';
                     this.descripcion='Premio de 3000 pesos ';
-                  } 
+                  }
 
 
-              
+
             }else{
               this.premioCliente='0';  // por defecto si el cliente esta registrado se le asigna el de sigue participando
               this.premio='0';
               this.descripcion='Cliente ya registrado en prospectos';
             }
-              
+
               this.completaPremio(folio);
 
     },
@@ -474,7 +476,7 @@ export default {
                   'nombres':me.no,
                   'apep':me.app,
                   'apem':me.apm,
-                  'fenac':me.fn,                  
+                  'fenac':me.fn,
               }).then(function(response){
                   var respuesta=response.data;
                   me.folio=respuesta.folio;
@@ -486,7 +488,7 @@ export default {
 
               });
     },
-    completaPremio(folio){ // completa los campos faltantes de el premio segun su folio asignado 
+    completaPremio(folio){ // completa los campos faltantes de el premio segun su folio asignado
             let me = this;
                 axios.put('datosPremio/completa', {
                   'id':folio,
@@ -534,7 +536,7 @@ export default {
     },
     checkForm(){  // valida el formato de correo que sea valido
             if (this.formatoCorreo.test(this.reg_correo) || this.btn_waths){
-              
+
               return true;
             }else{
               this.validCorreo=true;
@@ -543,8 +545,8 @@ export default {
     },
 
   },
-   
-    
+
+
 
 }
 
@@ -582,8 +584,8 @@ body{
   background-image: url('/img/ruleta/Background.png');
   background-position: center;
   width: 100%;
-  height: 100%; 
-  z-index: -2; 
+  height: 100%;
+  z-index: -2;
 }
 
   .top{
@@ -612,29 +614,29 @@ body{
 .div_fanfar_izq{
   grid-column: 1/6;
   grid-row:2/13;
-  width: 540px;height: 550px; 
+  width: 540px;height: 550px;
   z-index:0;
-  
-  
+
+
 }
 .div_fanfar_der{
   grid-column: 12/17;
   grid-row:2/13;
-  width: 540px;height: 550px; 
+  width: 540px;height: 550px;
   z-index: 0;
-  
-  
+
+
 }
 
 
-.div_tr{   
+.div_tr{
   grid-column: 11;
   grid-row:9;
   z-index: 2;
   width: 60px;
   height: 50px;
   /* margin-top: 20px; */
-    /* si se desea alineacion dentro de este contenedor se añaden reglas de flexbox*/    
+    /* si se desea alineacion dentro de este contenedor se añaden reglas de flexbox*/
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -651,7 +653,7 @@ body{
   animation-timing-function:linear;
   animation-iteration-count:infinite;
   animation-play-state:running;
-  
+
 
 }
 
@@ -673,12 +675,12 @@ body{
   grid-row:19/21;
   width: 180px;
   height: 100px;
-    /* si se desea alineacion dentro de este contenedor se añaden reglas de flexbox*/    
+    /* si se desea alineacion dentro de este contenedor se añaden reglas de flexbox*/
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  
+
 }
 .img_girar{
     width: 180px;
@@ -693,7 +695,7 @@ body{
   grid-row:19;
   width: 420px;
   height: 50px;
-    /* si se desea alineacion dentro de este contenedor se añaden reglas de flexbox*/    
+    /* si se desea alineacion dentro de este contenedor se añaden reglas de flexbox*/
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -705,7 +707,7 @@ body{
   grid-row:20;
   width: 240px;
   height: 50px;
-    /* si se desea alineacion dentro de este contenedor se añaden reglas de flexbox*/    
+    /* si se desea alineacion dentro de este contenedor se añaden reglas de flexbox*/
   display: flex;
   flex-direction: row;
   justify-content:space-between;
@@ -713,7 +715,7 @@ body{
 }
 .img_enviar{
     margin-top: 40px;
-    
+
     width: 70px;
     height: 60px;
 
@@ -730,7 +732,7 @@ body{
 
   background-image: url('/img/ruleta/formulario1.png');
 
-    /* si se desea alineacion dentro de este contenedor se añaden reglas de flexbox*/    
+    /* si se desea alineacion dentro de este contenedor se añaden reglas de flexbox*/
   display: flex;
   flex-direction: column;
   justify-content:center;
@@ -751,8 +753,8 @@ body{
     color: aliceblue;
     font-size: x-large;
     font-family: sans-serif;
-    
-    
+
+
 }
 
 .div_boton_gl{
@@ -761,7 +763,7 @@ body{
   width: 240px;
   height: 50px;
 
-    /* si se desea alineacion dentro de este contenedor se añaden reglas de flexbox*/    
+    /* si se desea alineacion dentro de este contenedor se añaden reglas de flexbox*/
   display: flex;
   flex-direction: column;
   justify-content:center;
@@ -782,7 +784,7 @@ body{
   background-image: url('/img/ruleta/formulario2.png');
   background-repeat: no-repeat;
 
-    /* si se desea alineacion dentro de este contenedor se añaden reglas de flexbox*/    
+    /* si se desea alineacion dentro de este contenedor se añaden reglas de flexbox*/
   display: flex;
   flex-direction: column;
   justify-content:center;
@@ -809,11 +811,11 @@ body{
 }
 .aviso_priv{
 
-  font-size: 20px; 
-  color: tan;
-  margin-top: 180px;
+  font-size: 20px;
+  color: yellow;
+  margin-top: 125px;
   margin-left: -10px;
-  
+
 }
 
 .aviso_priv:hover{
@@ -824,13 +826,13 @@ body{
 
 .div_boton_cw{
   grid-column: 10/13;
-  grid-row: 19;
+  grid-row: 18;
   width: 140px;
   height: 50px;
 
     margin-left: 10px;
 
-    /* si se desea alineacion dentro de este contenedor se añaden reglas de flexbox*/    
+    /* si se desea alineacion dentro de este contenedor se añaden reglas de flexbox*/
   display: flex;
   flex-direction: column;
   justify-content:center;
@@ -854,7 +856,7 @@ body{
     animation-iteration-count:infinite;
     animation-play-state:running;
 
-    
+
 
   }
   .mensaje-error{
@@ -863,131 +865,131 @@ body{
       left: 300px;
       padding-top: 30px;
       margin-left: 100px;
-      
+
   }
 
 
 
-     @keyframes flecha {  
+     @keyframes flecha {
       from{
            -moz-transform: rotate(0deg);
            -webkit-transform: rotate(0deg);
            -ms-transform: rotate(0deg);
-           
+
       }
-      to { 
-          -moz-transform: rotate(-15deg); 
-          -webkit-transform: rotate(-15deg); 
-          -ms-transform: rotate(-15deg); 
-        
+      to {
+          -moz-transform: rotate(-15deg);
+          -webkit-transform: rotate(-15deg);
+          -ms-transform: rotate(-15deg);
+
         }
 
-	
+
 }
-     @keyframes carousel {  
+     @keyframes carousel {
       from{
         -moz-transform: rotate(0deg);
         -webkit-transform: rotate(0deg);
-        -ms-transform: rotate(0deg); 
-        
+        -ms-transform: rotate(0deg);
+
         }
       to {
-        -moz-transform: rotate(360deg); 
-        -webkit-transform: rotate(360deg); 
-        -ms-transform: rotate(360deg); 
-        
+        -moz-transform: rotate(360deg);
+        -webkit-transform: rotate(360deg);
+        -ms-transform: rotate(360deg);
+
         }
 
-	
+
 }
-     @keyframes carousel8 {  
-    from { 
-      -moz-transform: rotate(0deg); 
-      -webkit-transform: rotate(0deg); 
-      -ms-transform: rotate(0deg); 
-      
+     @keyframes carousel8 {
+    from {
+      -moz-transform: rotate(0deg);
+      -webkit-transform: rotate(0deg);
+      -ms-transform: rotate(0deg);
+
       }
-    to { 
-      -moz-transform: rotate(7230deg); 
-      -webkit-transform: rotate(7230deg); 
-      -ms-transform: rotate(7230deg); 
-      
+    to {
+      -moz-transform: rotate(7230deg);
+      -webkit-transform: rotate(7230deg);
+      -ms-transform: rotate(7230deg);
+
       }
 
-	
+
 }
-     @keyframes carousel5 {  
-    from { 
-      -moz-transform: rotate(0deg); 
-      -webkit-transform: rotate(0deg); 
-      -ms-transform: rotate(0deg); 
-      
+     @keyframes carousel5 {
+    from {
+      -moz-transform: rotate(0deg);
+      -webkit-transform: rotate(0deg);
+      -ms-transform: rotate(0deg);
+
       }
-    to { 
+    to {
       -moz-transform: rotate(7260deg);
       -webkit-transform: rotate(7260deg);
       -ms-transform: rotate(7260deg);
-      
+
       }
 
-	
+
 }
-     @keyframes carousel3 {  
-    from { 
+     @keyframes carousel3 {
+    from {
       -moz-transform: rotate(0deg);
       -webkit-transform: rotate(0deg);
       -ms-transform: rotate(0deg);
       }
-    to { 
+    to {
       -moz-transform: rotate(7290deg);
       -webkit-transform: rotate(7290deg);
       -ms-transform: rotate(7290deg);
       }
-	
+
 }
-     @keyframes carousel0 {  
-    from { 
-      -moz-transform: rotate(0deg); 
-      -webkit-transform: rotate(0deg); 
-      -ms-transform: rotate(0deg); 
-      
+     @keyframes carousel0 {
+    from {
+      -moz-transform: rotate(0deg);
+      -webkit-transform: rotate(0deg);
+      -ms-transform: rotate(0deg);
+
       }
-    to { 
+    to {
       -moz-transform: rotate(7320deg);
       -webkit-transform: rotate(7320deg);
       -ms-transform: rotate(7320deg);
       }
 
-	
+
 }
-     @keyframes carousel15 {  
-    from { 
+     @keyframes carousel15 {
+    from {
       -moz-transform: rotate(0deg);
       -webkit-transform: rotate(0deg);
       -ms-transform: rotate(0deg);
-      
+
       }
-    to { 
+    to {
       -moz-transform: rotate(7350deg);
       -webkit-transform: rotate(7350deg);
       -ms-transform: rotate(7350deg);
       }
 
-	
+
 }
-     @keyframes carousel10 {  
-    from { 
-      -moz-transform: rotate(0deg); 
-      --webkit-transform: rotate(0deg); 
-      --ms-transform: rotate(0deg); 
+     @keyframes carousel10 {
+    from {
+      -moz-transform: rotate(0deg);
+      --webkit-transform: rotate(0deg);
+      --ms-transform: rotate(0deg);
       }
-    to { 
+    to {
       -moz-transform: rotate(7380deg);
       --webkit-transform: rotate(7380deg);
       --ms-transform: rotate(7380deg);
        }
 
-	
+
 }
 
 
